2fa493c4207975984b9424680c2cacd8935f05ea
zhangzexin <zzx-nuc@qq.com>
2024-04-05 22:29:41 +0800

    调整代码样式

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,76 @@
 <template>
-  <div>
-    <pre><code class="language-sql line-numbers">{{sql}}</code></pre>
-    <vxe-table
-        border
-        show-overflow
-        ref="xTable"
-        :column-config="{resizable: true}"
-        :loading="loading"
-        :data="tableData"
-        :edit-config="{trigger: 'manual', mode: 'row'}">
-      <vxe-column type="seq" width="60"></vxe-column>
-      <vxe-column field="columnName" title="字段名" :edit-render="{}">
-        <template #edit="{ row }: { row:RowVO}">
-          <vxe-input v-model="row.columnName" type="text"></vxe-input>
-        </template>
-      </vxe-column>
-      <vxe-column field="columnType" title="字段类型" :edit-render="{}">
-        <template #edit="{ row }: { row:RowVO}">
-          <vxe-input v-model="row.columnType" type="text" placeholder="请输入字段类型"></vxe-input>
-        </template>
-      </vxe-column>
-      <vxe-column field="isPrimaryKey" title="是否主键" :edit-render="{}">
-        <template #default="{ row } : { row:RowVO}">
-          <span>{{ formatBoolean(row.isPrimaryKey) }}</span>
-        </template>
-        <template #edit="{ row } : { row:RowVO}">
-          <vxe-select v-model="row.isPrimaryKey" transfer>
-            <vxe-option v-for="item in booleanList" :key="item.value" :value="item.value" :label="item.label"></vxe-option>
-          </vxe-select>
-        </template>
-      </vxe-column>
-      <vxe-column field="isAutoIncrement" title="是否自增id" :edit-render="{}">
-        <template #edit="{ row }: { row:RowVO}">
-          <vxe-input v-model="row.isAutoIncrement" type="text" placeholder="请输入是否主键"></vxe-input>
-        </template>
-      </vxe-column>
-      <vxe-column field="length" title="长度" :edit-render="{}">
-        <template #edit="{ row }: { row:RowVO}">
-          <vxe-input v-model="row.length" type="text" placeholder="请输入长度"></vxe-input>
-        </template>
-      </vxe-column>
-      <vxe-column field="nullable" title="是否为空" :edit-render="{}">
-        <template #edit="{ row }: { row:RowVO}">
-          <vxe-input v-model="row.nullable" type="text" placeholder="是否为空"></vxe-input>
-        </template>
-      </vxe-column>
-      <vxe-column field="remarks" title="备注" :edit-render="{}">
-        <template #edit="{ row }: { row:RowVO}">
-          <vxe-input v-model="row.remarks" type="text" placeholder="备注"></vxe-input>
-        </template>
-      </vxe-column>
-      <vxe-column field="defaultValue" title="默认值" :edit-render="{}">
-        <template #edit="{ row }: { row:RowVO}">
-          <vxe-input v-model="row.defaultValue" type="text" placeholder="默认值"></vxe-input>
-        </template>
-      </vxe-column>
-      <vxe-column title="操作" width="160">
-        <template #default="{ row }">
-          <template v-if="isActiveStatus(row)">
-            <vxe-button @click="saveRowEvent(row)">保存</vxe-button>
-            <vxe-button @click="cancelRowEvent(row)">取消</vxe-button>
+  <div class="container">
+    <div class="table">
+      <vxe-table
+          border
+          show-overflow
+          ref="xTable"
+          :column-config="{resizable: true}"
+          :loading="loading"
+          :data="tableData"
+          :edit-config="{trigger: 'manual', mode: 'row'}">
+        <vxe-column type="seq" width="60"></vxe-column>
+        <vxe-column field="columnName" title="字段名" :edit-render="{}">
+          <template #edit="{ row }: { row:RowVO}">
+            <vxe-input v-model="row.columnName" type="text"></vxe-input>
           </template>
-          <template v-else>
-            <vxe-button @click="editRowEvent(row)">编辑</vxe-button>
+        </vxe-column>
+        <vxe-column field="columnType" title="类型" :edit-render="{}">
+          <template #edit="{ row }: { row:RowVO}">
+            <vxe-input v-model="row.columnType" type="text" placeholder="请输入字段类型"></vxe-input>
           </template>
-        </template>
-      </vxe-column>
-    </vxe-table>
+        </vxe-column>
+        <vxe-column field="isPrimaryKey" title="主键" :edit-render="{}">
+          <template #default="{ row } : { row:RowVO}">
+            <span>{{ formatBoolean(row.isPrimaryKey) }}</span>
+          </template>
+          <template #edit="{ row } : { row:RowVO}">
+            <vxe-select v-model="row.isPrimaryKey" transfer>
+              <vxe-option v-for="item in booleanList" :key="item.value" :value="item.value" :label="item.label"></vxe-option>
+            </vxe-select>
+          </template>
+        </vxe-column>
+        <vxe-column field="isAutoIncrement" title="自增id" :edit-render="{}">
+          <template #edit="{ row }: { row:RowVO}">
+            <vxe-input v-model="row.isAutoIncrement" type="text" placeholder="请输入是否主键"></vxe-input>
+          </template>
+        </vxe-column>
+        <vxe-column field="length" title="长度" :edit-render="{}">
+          <template #edit="{ row }: { row:RowVO}">
+            <vxe-input v-model="row.length" type="text" placeholder="请输入长度"></vxe-input>
+          </template>
+        </vxe-column>
+        <vxe-column field="nullable" title="是否为空" :edit-render="{}">
+          <template #edit="{ row }: { row:RowVO}">
+            <vxe-input v-model="row.nullable" type="text" placeholder="是否为空"></vxe-input>
+          </template>
+        </vxe-column>
+        <vxe-column field="remarks" title="备注" :edit-render="{}">
+          <template #edit="{ row }: { row:RowVO}">
+            <vxe-input v-model="row.remarks" type="text" placeholder="备注"></vxe-input>
+          </template>
+        </vxe-column>
+        <vxe-column field="defaultValue" title="默认值" :edit-render="{}">
+          <template #edit="{ row }: { row:RowVO}">
+            <vxe-input v-model="row.defaultValue" type="text" placeholder="默认值"></vxe-input>
+          </template>
+        </vxe-column>
+        <vxe-column title="操作" width="160">
+          <template #default="{ row }">
+            <template v-if="isActiveStatus(row)">
+              <vxe-button @click="saveRowEvent(row)">保存</vxe-button>
+              <vxe-button @click="cancelRowEvent(row)">取消</vxe-button>
+            </template>
+            <template v-else>
+              <vxe-button @click="editRowEvent(row)">编辑</vxe-button>
+            </template>
+          </template>
+        </vxe-column>
+      </vxe-table>
+    </div>
+    <div class="sql">
+      <pre><code class="language-sql line-numbers">{{sql}}</code></pre>
+    </div>
   </div>
 </template>
 
@@ -160,3 +164,23 @@ const cancelRowEvent = (row: RowVO) => {
   }
 }
 </script>
+
+<style scoped>
+/* 容器 div */
+.container {
+  display: flex; /* 使用 Flex 布局实现简洁的两列布局 */
+  justify-content: space-between; /* 在主轴方向上均匀分配间距，让两个子元素两端对齐 */
+  width: 100%; /* 确保容器填满其父元素的宽度 */
+}
+
+/* 子 div，每个占 50% 宽度 */
+.container > .table {
+  flex: 0 0 55%; /* 设置flex-grow(增长比例)、flex-shrink(收缩比例)和flex-basis(基础大小)，确保固定为50%宽度 */
+  box-sizing: border-box; /* 包含 padding 和 border 在计算宽度之内，避免因边距或边框导致总宽度超出100% */
+}
+
+.container > .sql {
+  flex: 0 0 44%; /* 设置flex-grow(增长比例)、flex-shrink(收缩比例)和flex-basis(基础大小)，确保固定为50%宽度 */
+  box-sizing: border-box; /* 包含 padding 和 border 在计算宽度之内，避免因边距或边框导致总宽度超出100% */
+}
+</style>
